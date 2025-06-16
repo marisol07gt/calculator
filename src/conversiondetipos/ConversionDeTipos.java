f804ce59d45097869d8a2d45e8cf37d2e53e3a38
marisol07gt <l.togm0601007@itses.edu.mx>
2025-06-16 10:07:05 -0600

    cambio en archivo conversion tipos

--- src/conversiondetipos/ConversionDeTipos.java
+++ src/conversiondetipos/ConversionDeTipos.java
@@ -14,7 +14,21 @@ public class ConversionDeTipos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("holaaaaaaa");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
+    
     
 }
